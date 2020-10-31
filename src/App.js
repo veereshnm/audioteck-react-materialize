5dcba1cb226989754c986bfca7b49492d46bf7ae
Veeresh Nerlige <veereshnm@gmail.com>
2020-10-30 21:44:01 -0500

    changed the menu to audioteck - intial draft

--- src/App.js
+++ src/App.js
@@ -13,22 +13,22 @@ function App() {
     <div className="App">
       <nav>
     <div className="nav-wrapper">
-      <a href="#!" className="brand-logo">Logo</a>
+      <a href="#!" className="brand-logo">Audioteck</a>
       <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
       <ul className="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Product</a></li>
+        <li><a href="">Photos</a></li>
+        <li><a href="">Contact</a></li>
       </ul>
     </div>
   </nav>
 
   <ul className="sidenav" id="mobile-demo">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">Javascript</a></li>
-    <li><a href="mobile.html">Mobile</a></li>
+    <li><a href="">About</a></li>
+    <li><a href="">Product</a></li>
+    <li><a href="">Photos</a></li>
+    <li><a href="">Contact</a></li>
   </ul>
     </div>
   );
